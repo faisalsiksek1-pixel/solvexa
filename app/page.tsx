1b0b2161673765274dbea786b4f63339739c1e00
FBZAS <faisalooloo@icloud.com>
2026-06-30 14:41:53 -0400

    redesign landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-import { Navbar } from "@/components/layout/Navbar";
-import { Footer } from "@/components/layout/Footer";
 import { Hero } from "@/components/landing/Hero";
-import { CTA } from "@/components/landing/CTA";
 
 export default function LandingPage() {
   return (
-    <div className="min-h-screen bg-white">
-      <Navbar />
-      <main>
-        <Hero />
-        <CTA />
-      </main>
-      <Footer />
-    </div>
+    <main className="min-h-screen bg-[#07061a]">
+      <Hero />
+    </main>
   );
 }
